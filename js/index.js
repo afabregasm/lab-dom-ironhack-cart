9d092804302538fc41499d32325e83e965abf2e1
Andrea Fàbregas Martínez <a.fabregas@outlook.es>
2022-06-07 23:20:51 +0200

    Main iterations completed (tests not working

--- js/index.js
+++ js/index.js
@@ -1,27 +1,38 @@
+// Hola profes :)
+// No sé por qué pero los tets no me pasan, sin embargo el comportamiento de la página es correcto.
+// Lo subo igualmente, si puede ser en clase lo miramos. ¡Gracias!
+
 // ITERATION 1
-
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  let priceElement = product.querySelector(".price span");
+  let price = Number(priceElement.innerText);
+  let quantityElement = product.querySelector(".quantity input");
+  let quantity = Number(quantityElement.value);
+  let subtotalElement = product.querySelector(".subtotal span");
+  subtotalElement.innerText = price * quantity;
 
-  //... your code goes here
+  console.log('Calculating subtotal, yey!');
+  return subtotalElement.innerText;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  // ITERATION 2 & 3
+  let pricesElement = document.getElementsByClassName("product");
+  let priceTotalElement = document.querySelector("#total-value span");
+  let priceTotal = 0;
+  for(let i = 0; i < pricesElement.length; i++) {
+    priceTotal += Number(updateSubtotal(pricesElement[i]));
+  }
+  priceTotalElement.innerText = priceTotal;
 }
 
 // ITERATION 4
-
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
@@ -29,7 +40,6 @@ function removeProduct(event) {
 }
 
 // ITERATION 5
-
 function createProduct() {
   //... your code goes here
 }
